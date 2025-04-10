7b49255ce17ac7f0a8487a3ac69727a405365f5c
AnanyaGanji <139428449+AnanyaGanji@users.noreply.github.com>
2025-04-10 19:37:07 +0530

    Created Student.h file

---
+++ Student.h
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Student {
+    public:
+    int rollNo;
+    string name;
+    string course;
+    float marks;
+
+    Student(int rollNo, string name, string course, float marks) : rollNo(rollNo), name(name), course(course), marks(marks)
+    {}
+
+    void display() {
+        cout << "Student Detials:\nRoll no: " << rollNo << "\nName: " << name << "\nCourse: " << course << "\nMarks: " << marks << "\n"<< endl;
+    }
+    
+};
